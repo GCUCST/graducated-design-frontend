442c293a98798afe45014af0707e5dbe499093b7
叶吉利 <yjlally@163.om>
2020-01-18 13:34:24 +0800

    1-18,13:33，更新考试系统课程与试卷设置

--- src/components/exam_system/Courses.vue
+++ src/components/exam_system/Courses.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="courses">
-      {{msg}}
+     <h3>我的课程</h3>
+      <div class="content">
+        <el-card
+        shadow="hover"
+        v-for="i in 3"
+        :key="i"
+        class="box-card">
+          <img src="https://gss2.bdstatic.com/-fo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=3fc712b65c4e9258b2398ebcfdebba3d/8718367adab44aed18974436b91c8701a08bfb03.jpg" class="image">
+          <div class="cour-name">
+          <span @click="routeJump('TestSetting')">计算机网络</span>
+          </div>
+        </el-card>
+      </div>
+      <div class="clean"></div>
+      <div class="page">
+          <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+      </div>
   </div>
 </template>
 
@@ -12,6 +28,13 @@ export default {
     return {
       msg: "考试系统-课程",
     };
+  },
+  methods:{
+    routeJump(e) {
+      console.log(e);
+
+      if (e == "TestSetting") this.$router.push({ name: "TestSetting" });
+    }
   }
 };
 </script>
@@ -25,4 +48,30 @@ export default {
   height: 600px;
   box-shadow: 0px 0px 10px 6px rgba(0, 0, 0, 0.1);
 }
+.content{
+   display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  cursor:pointer
+}
+.image{
+  width: 235px;
+  height: 236px;
+}
+.cour-name{
+  padding: 15px;
+  font-size: 20px;
+}
+.clean{
+  clear: both;
+}
+.box-card{
+  width: 300px;
+  margin:2%;
+}
+.page{
+  text-align:center;
+  margin-top: 30px;
+}
+
 </style>
--- src/components/exam_system/TestSetting.vue
+++ src/components/exam_system/TestSetting.vue
@@ -1,6 +1,42 @@
 <template>
   <div class="testsetting">
-      {{msg}}
+      <h3>试卷设置</h3>
+      <el-form ref="form" :model="form" label-width="80px">
+  <el-form-item label="试卷名称">
+    <el-input v-model="form.name" placeholder="请输入试卷名称"></el-input>
+  </el-form-item>
+  <el-form-item label="试卷总分">
+    <el-input v-model="form.score" placeholder="请输入试卷总分"></el-input>
+  </el-form-item>
+  <el-form-item label="可用次数">
+    <el-input v-model="form.num" placeholder="试卷可用次数"></el-input>
+  </el-form-item>
+  <el-form-item label="考试时间">
+    <el-col :span="11">
+      <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+    </el-col>
+    <el-col class="line" :span="2">-</el-col>
+    <el-col :span="11">
+      <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+    </el-col>
+  </el-form-item>
+
+  <el-form-item label="试卷题型">
+    <el-checkbox-group v-model="form.type">
+      <el-checkbox label="选择题" name="type"></el-checkbox>
+      <el-checkbox label="填空题" name="type"></el-checkbox>
+      <el-checkbox label="简单题" name="type"></el-checkbox>
+      <el-checkbox label="编程题" name="type"></el-checkbox>
+      <el-checkbox label="程序填空" name="type"></el-checkbox>
+    </el-checkbox-group>
+  </el-form-item>
+
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">创建试卷</el-button>
+    <el-button>取消</el-button>
+  </el-form-item>
+</el-form>
+
   </div>
 </template>
 
@@ -11,8 +47,21 @@ export default {
   data() {
     return {
       msg: "考试系统-试卷设置",
+      form: {
+        name: '',
+        score: '',
+        num:'',
+        date1: '',
+        date2: '',
+        type: [],
+      }
     };
-  }
+  },
+  methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    }
 };
 </script>
 
