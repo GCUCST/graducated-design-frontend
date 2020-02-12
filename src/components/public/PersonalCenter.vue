9f6b8a632ff30c0c1e26cb4e555af14195484f15
陈少桐 <1528841402@qq.com>
2020-02-12 21:24:43 +0800

    2-12  更新

--- src/components/public/PersonalCenter.vue
+++ src/components/public/PersonalCenter.vue
@@ -2,8 +2,7 @@
   <div class="personal-center">
        <el-tabs tab-position="up">
     <el-tab-pane label="我的信息">
-       账号：{{user.account}}<br>
-       身份：{{user.role=='dean'?'教务员':user.role=='teacher'?'教师':user.role=='student'?'学生':'管理员'}}
+      <v-MyInfo></v-MyInfo>
     </el-tab-pane>
     <el-tab-pane label="我的头像">我的头像 </el-tab-pane>
     <el-tab-pane label="账户安全">账户安全</el-tab-pane>
@@ -13,13 +12,17 @@
 
 
 <script>
+import MyInfo from "@/components/public/personal_center/MyInfo.vue"
 export default {
   name: "PersonalCenter",
   data() {
     return {
-      msg: "个人中心",
-      user:JSON.parse(localStorage.getItem("userInfo"))
+      msg: "个人中心"
+   
     };
+  },
+  components:{
+    "v-MyInfo":MyInfo
   }
 };
 </script>
@@ -30,7 +33,6 @@ export default {
   padding: 30px;
   background: white;
   width: 95%;
-  height: 600px;
   box-shadow: 0px 0px 10px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
---
+++ src/components/public/personal_center/MyInfo.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    账号：{{user.account}}<br />
+    身份：{{user.role=='dean'?'教务员':user.role=='teacher'?'教师':user.role=='student'?'学生':'管理员'}}<br>
+    <div v-if="user.role=='teacher'">
+    昵称：{{user.info.nickName}}<br>
+    姓名：{{user.info.name}}<br>
+    性别：{{user.info.gender==1?'男':'女'}}<br>
+    邮箱：{{user.info.email}}<br>
+    手机号：{{user.info.phone}}<br>
+    身份：{{user.info.idcard}}<br>
+    学历：{{user.info.education}}<br>
+    职称：{{user.info.technicalTitle}}<br>
+    部门：{{user.info.dept}}<br>
+    教工状态：{{user.info.status}}<br>
+    创建时间：{{user.info.createTime}}<br>
+    通讯地址：{{user.info.city}}<br>
+    入职时间：{{user.info.admissionDate}}<br>
+    政治面貌：{{user.info.political}}<br>
+    出生日期：{{user.info.birthday}}<br>
+    年龄：{{user.info.age}}<br>
+    宿舍：{{user.info.dorm}}<br>
+    </div>
+    <div v-if="user.role=='student'">
+    昵称：{{user.info.nickName}}<br>
+    姓名：{{user.info.name}}<br>
+    性别：{{user.info.gender==1?'男':'女'}}<br>
+    邮箱：{{user.info.email}}<br>
+    手机号：{{user.info.phone}}<br>
+    身份：{{user.info.idcard}}<br>
+    教工状态：{{user.info.status}}<br>
+    创建时间：{{user.info.createTime}}<br>
+    通讯地址：{{user.info.city}}<br>
+    入职时间：{{user.info.admissionDate}}<br>
+    政治面貌：{{user.info.political}}<br>
+    出生日期：{{user.info.birthday}}<br>
+    年龄：{{user.info.age}}<br>
+    宿舍：{{user.info.dorm}}<br>
+    专业：{{user.info.major}}<br>
+    行政班级：{{user.info.adminClass}}<br>
+    年级：{{user.info.grade}}<br>
+    系别：{{user.info.secondaryCollege}}<br>
+    </div>
+
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "myinfo",
+  data() {
+    return {
+      msg: "个人中心",
+      user: JSON.parse(localStorage.getItem("userInfo"))
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
